c41ad18b9c744d2af1e577afd300fa9fb2bdf1e9
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-08 17:07:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  holiday_hash.map.each do |season, holiday_hash|
+  end
 end
 
 
